931e6df24fa79adcd8efc7009932e320e39c00cd
Danladi <galadimad892@gmail.com>
2022-07-08 13:12:00 +0100

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+const {DateTime} = require("luxon");
+
+
+module.exports = function(eleventyConfig){
+    eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPassthroughCopy('./src/assets/');
+    eleventyConfig.addFilter("postDate", (dateObj)=>{
+ return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    });
+    return {
+        dir: {
+            input: 'src',
+            includes: '_includes',
+            output: 'public'
+        },
+        templateFormats: ['md', 'njk', 'html'],
+        markdownTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk'
+    };
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+#JAMstack static site blog
